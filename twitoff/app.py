1e5978c7a9c9f4ced60eddca380e5fd182d9d631
BadaS <jamessopkin@gmail.com>
2020-12-17 12:15:08 -0600

    prepare app for deployment

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,23 +1,76 @@
-# main app/routing file for twitoff
+"""Main app/routing file for Twitoff"""
 
-from flask import Flask
-from .models import DB, User, Tweet
+from os import getenv
+from flask import Flask, render_template, request
+from .twitter import add_or_update_user
+from .models import DB, User, MIGRATE
+from .predict import predict_user
+
 
 def create_app():
     app = Flask(__name__)
 
-    app.config("SQLAlchemy_DATABASE_URI") = "sqlite:///db.sqlite3"
-    app.config("SQLAlCHEMY_TRACK_MODIFICATIONS") = False
+    app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    DB.init_app(app)
+    MIGRATE.init_app(app, DB)
+
+    # TODO - make rest of application
 
     @app.route('/')
     def root():
-        # SQl equivalent = "SELECT * FROM user;"
-        user = User.query.all()
-        return render_template(base.html, title="Home", user=users)
+        # SQL equivalent = "SELECT * FROM user;"
+        return render_template('base.html', title="Home", users=User.query.all())
 
-    @app.route('/reset')
+    @app.route("/compare", methods=["POST"])
+    def compare():
+        user0, user1 = sorted(
+            [request.values["user1"], request.values["user2"]])
+
+        # conditinoal that prevents same user comparison
+        if user0 == user1:
+            message = "Cannot compare users to themselves!"
+
+        else:
+            hypo_tweet_text = request.values["tweet_text"]
+            # prediction return zero or one depending upon user
+            prediction = predict_user(user0, user1, hypo_tweet_text)
+            message = "'{}' is more likely to be said by {} than {}".format(
+                hypo_tweet_text, user1 if prediction else user0,
+                user0 if prediction else user1
+            )
+
+        # returns rendered template with dynamic message
+        return render_template('prediction.html', title="Prediction:", message=message)
+
+    @app.route("/user", methods=["POST"])
+    @app.route("/user/<name>", methods=["GET"])
+    def user(name=None, message=""):
+        name = name or request.values["user_name"]
+        try:
+            if request.method == "POST":
+                add_or_update_user(name)
+                message = "User {} sucessfully added!".format(name)
+
+            tweets = User.query.filter(User.name == name).one().tweets
+
+        except Exception as e:
+            message = "Error handling {}: {}".format(name, e)
+            tweets = []
+
+        return render_template("user.html", title=name, tweets=tweets, message=message)
+
+    @app.route("/update")
+    def update():
+        users = User.query.all()
+        for user in users:
+            add_or_update_user(user.name)
+        return render_template("base.html", title="Database has been updated!", users=User.query.all())
+
+    @app.route("/reset")
     def reset():
-        return "reset endpoint"
-
+        DB.drop_all()
+        DB.create_all()
+        return render_template("base.html", title="Reset Database")
 
     return app
--- twitoff/models.py
+++ twitoff/models.py
@@ -1,26 +1,38 @@
 """SQLAlchemy models and utility functions for Twitoff"""
 
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 DB = SQLAlchemy()
+MIGRATE = Migrate()
 
+
+# User Table (in relational database the table is "user")
 class User(DB.Model):
     """Twitter users corresponding to Tweets"""
-    #primary id column
+    # primary id column
     id = DB.Column(DB.BigInteger, primary_key=True)
-    #name column
+    # name column
     name = DB.Column(DB.String, nullable=False)
+    # keeps track of users most recent tweet
+    newest_tweet_id = DB.Column(DB.BigInteger)
 
     def __repr__(self):
         return "<User: {}>".format(self.name)
 
-# Tweet Table
+
+# Tweet Table (in relational database the table is "tweet")
 class Tweet(DB.Model):
+    """Tweet Text and Data"""
     # primary id column
-    id = DB.Column(Db.BigInteger, primary_key=True)
+    id = DB.Column(DB.BigInteger, primary_key=True)
     # text column of character length 300 (unicode)
     text = DB.Column(DB.Unicode(300))
-    # foreign key - user id
+    vect = DB.Column(DB.PickleType, nullable=False)
+    # foreign key - user.id
     user_id = DB.Column(DB.BigInteger, DB.ForeignKey(
         'user.id'), nullable=False)
-    user = DB.relationship("User", backref=DB.backref('tweets', lazy=True))
+    user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
+
+    def __repr__(self):
+        return "<Tweet: {}>".format(self.text)
